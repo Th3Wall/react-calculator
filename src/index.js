e1900e6c8e8495e4d37ea87fa31c0450a9bfefab
Th3Wall <mandellidavide.95@gmail.com>
2021-01-10 23:15:31 +0100

    added github corner

--- src/index.js
+++ src/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 import './_variables.sass';
 import './index.sass';
 import Calculator from './Calculator';
+import GithubCorner from 'react-github-corner';
+
+const customHref = 'https://github.com/Th3Wall';
 
 ReactDOM.render(
   <React.StrictMode>
     <Calculator />
+    <GithubCorner 
+      href={customHref}
+      bannerColor="#ffffff"
+      octoColor="#82C874"
+      size={80}
+      direction="right"
+    />
   </React.StrictMode>,
   document.getElementById('root')
 );
